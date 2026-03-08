7fd2cd2b2afa322bbb0bd447a3956d13a542eedd
Ayush <2005.ayushk@gmail.com>
2026-03-08 18:26:29 +0530

    test v last

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,6 +91,11 @@ resource "aws_instance" "devops_server" {
   vpc_security_group_ids      = [aws_security_group.devops_sg.id]
   associate_public_ip_address = true
 
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
   key_name = "my-key"  # must exist in us-east-1
 
   tags = { Name = "DevOps-Lab-Server" }
